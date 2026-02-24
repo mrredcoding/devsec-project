c4092e19491f3aa9799944115ca5a99394abb7a8
mrredcoding <cedric.alonso.pro@gmail.com>
2026-02-24 01:24:18 +0100

    Use secure api endpoint

--- bank-frontend/src/gateway/api.ts
+++ bank-frontend/src/gateway/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5000",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "https://localhost:5000",
 });
 
 api.interceptors.request.use((config) => {
