4768034d7e6341d425dd6508217a6c806d83880b
mrredcoding <cedric.alonso.pro@gmail.com>
2026-02-24 01:13:31 +0100

    Remove useless authentication parameter

--- bank-backend/src/main/java/efrei/bankbackend/configuration/components/AccountComponent.java
+++ bank-backend/src/main/java/efrei/bankbackend/configuration/components/AccountComponent.java
@@ -1,12 +1,17 @@
 package efrei.bankbackend.configuration.components;
 
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
+
+import java.util.Objects;
 
 @Component("accountComponent")
 public class AccountComponent {
 
-    public boolean isOwner(String owner, Authentication authentication) {
+    public boolean isOwner(String owner) {
+        Authentication authentication = Objects.requireNonNull(SecurityContextHolder.getContext().getAuthentication());
+
         return owner.equals(authentication.getName());
     }
 }
